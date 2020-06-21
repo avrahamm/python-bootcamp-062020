eb296570d9aefe539851966b2665affdd096f297
avrahamm <papaabramg@gmail.com>
2020-06-21 12:03:11 +0300

    fixed by Ynon note

--- lesson11/ex6.py
+++ lesson11/ex6.py
@@ -1,20 +1,31 @@
 from random import randint
 
-range_limit = 10  # 100
+
+def read_numeric_value_in_range(range_bottom, range_top):
+    while True:
+        try:
+            guess_num = int(input(f"Guess number in range [{range_bottom}, {range_top}]: "))
+            if guess_num not in range(range_bottom, range_top):
+                continue
+            return guess_num
+        except Exception:
+            continue
+
+
+range_limit = 11  # 101
 num = randint(1, range_limit)
 trials = 0
 guess = -1
 
-while True:
-    # I would prefer to write a function that reads stubbornly a numeric value
-    # in range and call it from here
-    # guess = read_numeric_value_in_range(1, range_limit)
-    # (instead of writing the try/except here)
-    try:
-        guess = int(input(f"Guess number in range [1,{range_limit}]: "))
-    except Exception:
-        continue
 
+# I would prefer to write a function that reads stubbornly a numeric value
+# in range and call it from here
+# guess = read_numeric_value_in_range(1, range_limit)
+# (instead of writing the try/except here)
+#
+# => Fixed
+while True:
+    guess = read_numeric_value_in_range(1, range_limit )
     trials += 1
     if guess == num:
         print(f"You won! The number is {num}, trials = {trials}")
