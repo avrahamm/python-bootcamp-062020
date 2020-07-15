e86d837e545fec484e222843bec72a0fb3e05347
avrahamm <papaabramg@gmail.com>
2020-07-15 18:37:48 +0300

    completed lesson 29

--- p2-lesson29/ex1.py
+++ p2-lesson29/ex1.py
@@ -5,7 +5,7 @@ class AddressBook:
     def __init__(self, filename):
         self.filename = filename
         self.fp = None
-        self.content = {"contacts": None}
+        self.contacts = None
 
     def __enter__(self):
         """
@@ -14,29 +14,41 @@ class AddressBook:
         print("__enter__")
         try:
             self.fp = open(self.filename, "r")
-        except Exception as e:
+        except FileNotFoundError as e:
             self.fp = open(self.filename, "x")
             self.fp.close()
             self.fp = open(self.filename, "r")
 
         try:
             data = json.load(self.fp)
-            self.content["contacts"] = data['contacts']
+            self.contacts = data['contacts']
         except Exception as e:
-            self.content["contacts"] = {}
+            self.contacts = {}
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print("__exit__")
-        with open(self.filename, 'w') as outfile:
-            json.dump(self.content, outfile)
-        self.fp.close()
+        if exc_type is None:
+            with open(self.filename, 'w') as outfile:
+                json.dump({"contacts": self.contacts}, outfile)
+                self.fp.close()
+        else:
+            self.fp.close()
+            raise IndexError
 
     def add(self, name, email):
-        self.content["contacts"][name] = email
+        self.contacts[name] = email
+        # To simulate raising exception
+        # raise IndexError("testing")
 
     def email(self, name):
-        return self.content["contacts"][name]
+        return self.contacts[name]
+
+    def __setitem__(self, name, email):
+        self.contacts[name] = email
+
+    def __getitem__(self, name):
+        return self.contacts[name]
 
     def __str__(self):
         with open(self.filename) as json_file:
@@ -56,3 +68,11 @@ print(ab)
 
 with AddressBook('contacts.json') as ab:
     print(ab.email('Eve'))
+
+# __setitem
+with AddressBook('contacts.json') as ab:
+    ab['Dan'] = 'dan@gmail.com'
+
+# __getitem
+with AddressBook('contacts.json') as ab:
+    print(ab['Dan'])
